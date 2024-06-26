45bd4f387898f3b50a3b61377b973129fefbb2b6
Cristopher Rondon Añaños <89040296+Cris200399@users.noreply.github.com>
2024-06-26 18:28:18 -0500

    c2

--- fastport/fastport/client-service/src/main/java/com/autoBrokers/client_service/ClientServiceApplication.java
+++ fastport/fastport/client-service/src/main/java/com/autoBrokers/client_service/ClientServiceApplication.java
@@ -3,12 +3,14 @@ package com.autoBrokers.client_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @EnableFeignClients(basePackages = "com.autoBrokers.client_service.feignclients")
 @ImportAutoConfiguration({FeignAutoConfiguration.class})
 @SpringBootApplication
+@EnableEurekaClient
 public class ClientServiceApplication {
 
 	public static void main(String[] args) {
--- fastport/fastport/contract-service/src/main/java/com/autoBrokers/contract_service/ContractServiceApplication.java
+++ fastport/fastport/contract-service/src/main/java/com/autoBrokers/contract_service/ContractServiceApplication.java
@@ -1,11 +1,13 @@
 package com.autoBrokers.contract_service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@ImportAutoConfiguration
 public class ContractServiceApplication {
 
 	public static void main(String[] args) {
